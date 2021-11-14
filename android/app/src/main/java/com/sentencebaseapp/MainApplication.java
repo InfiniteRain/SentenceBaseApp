63641bb1891d7f5f51d070bcc4a093d141fd96e7
InfiniteRain <lysenkodavid@gmail.com>
2021-11-14 16:03:40 +0200

    Install AnkiDroid API

--- android/app/src/main/java/com/sentencebaseapp/MainApplication.java
+++ android/app/src/main/java/com/sentencebaseapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.is343.reactnativeankidroid.AnkiDroidPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
