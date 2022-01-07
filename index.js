ed582fff0ca91e40e307960ab9800718f149bb8f
InfiniteRain <lysenkodavid@gmail.com>
2022-01-07 18:28:46 +0200

    Implement Google sign-in

--- index.js
+++ index.js
@@ -5,5 +5,11 @@
 import {AppRegistry} from 'react-native';
 import {App} from './src/App';
 import {name as appName} from './app.json';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+
+GoogleSignin.configure({
+  webClientId:
+    '890176304594-ibeba03tai3lvsrtin1t8iae7hfg4ga0.apps.googleusercontent.com',
+});
 
 AppRegistry.registerComponent(appName, () => App);
