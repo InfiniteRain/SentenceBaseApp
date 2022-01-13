6991ce0c6da3837151955fcdb2919da3d9816ce2
InfiniteRain <lysenkodavid@gmail.com>
2022-01-13 16:21:39 +0200

    Add mecab support

--- src/app-state-context.ts
+++ src/app-state-context.ts
@@ -6,14 +6,42 @@ export enum Page {
   PendingSentences,
 }
 
+export type MecabMorpheme = {
+  word: string;
+  pos: string;
+  pos_detail1: string;
+  pos_detail2: string;
+  pos_detail3: string;
+  conjugation1: string;
+  conjugation2: string;
+  dictionary_form: string;
+  reading?: string;
+  pronunciation?: string;
+};
+
+export type MecabMessage =
+  | {
+      type: 'initialized';
+      data: null;
+    }
+  | {
+      type: 'queryResult';
+      data: {
+        query: string;
+        result: MecabMorpheme[];
+      };
+    };
+
 export const AppStateContext = createContext<{
   currentPage: Page;
   setCurrentPage: (page: Page) => void;
   batch: SentenceEntry[];
   setBatch: (batch: SentenceEntry[]) => void;
+  mecabQuery: (query: string) => Promise<MecabMorpheme[]>;
 }>({
   currentPage: Page.MainMenu,
   setCurrentPage: (_page: Page) => {},
   batch: [],
   setBatch: (_batch: SentenceEntry[]) => {},
+  mecabQuery: async (_query: string) => [],
 });
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,40 +1,151 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import AuthUI from 'react-native-firebaseui-auth';
 import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
-import {AppStateContext, Page} from '../app-state-context';
+import {
+  AppStateContext,
+  MecabMessage,
+  MecabMorpheme,
+  Page,
+} from '../app-state-context';
 import Toast from 'react-native-toast-message';
 import {MainMenu} from './main-menu';
 import {SentenceEntry} from '../common';
 import {PendingSentences} from './pending-sentences';
+import {StyleSheet, View} from 'react-native';
+import WebView, {WebViewMessageEvent} from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  mecabWebViewContainer: {
+    position: 'absolute',
+  },
+  mecabWebView: {
+    width: 0,
+    height: 0,
+  },
+});
 
 export const App = () => {
+  let initPromiseResolve!: () => void;
+  const initPromise = new Promise<void>(resolve => {
+    initPromiseResolve = resolve;
+  });
+
   const [currentUser, setCurrentUser] =
     useState<FirebaseAuthTypes.User | null>();
   const [currentPage, setCurrentPage] = useState<Page>(Page.MainMenu);
   const [batch, setBatch] = useState<SentenceEntry[]>([]);
-
-  useEffect(
-    () =>
-      auth().onAuthStateChanged(async user => {
-        setCurrentUser(user);
-
-        let isLoggedIn = user !== null;
-
-        while (!isLoggedIn) {
-          try {
-            await AuthUI.signIn({
-              providers: ['email'],
-              allowNewEmailAccounts: true,
-              requireDisplayName: false,
-            });
-            isLoggedIn = true;
-          } catch {}
-        }
-      }),
-    [],
+  const [mecabWebView, setMecabWebView] = useState<WebView | null>(null);
+  const [mecabInitPromise] = useState<Promise<void>>(initPromise);
+  const [mecabInitPromiseResolve] = useState<() => () => void>(
+    () => initPromiseResolve,
+  );
+  const [mecabCache] = useState(
+    new Map<
+      string,
+      [(morphemes: MecabMorpheme[]) => void, Promise<MecabMorpheme[]>]
+    >(),
   );
 
+  const mecabInitRef = useRef<{
+    mecabInitPromise: Promise<void>;
+    mecabInitPromiseResolve: () => void;
+  }>({
+    mecabInitPromise,
+    mecabInitPromiseResolve,
+  });
+  const mecabWebViewRef = useRef<WebView | null>(null);
+  const mecabCacheRef =
+    useRef<
+      Map<
+        string,
+        [(morphemes: MecabMorpheme[]) => void, Promise<MecabMorpheme[]>]
+      >
+    >(mecabCache);
+
+  mecabInitRef.current = {
+    mecabInitPromise,
+    mecabInitPromiseResolve,
+  };
+  mecabWebViewRef.current = mecabWebView;
+  mecabCacheRef.current = mecabCache;
+
+  useEffect(() => {
+    return auth().onAuthStateChanged(async user => {
+      setCurrentUser(user);
+
+      let isLoggedIn = user !== null;
+
+      while (!isLoggedIn) {
+        try {
+          await AuthUI.signIn({
+            providers: ['email'],
+            allowNewEmailAccounts: true,
+            requireDisplayName: false,
+          });
+          isLoggedIn = true;
+        } catch {}
+      }
+    });
+  }, []);
+
+  const onMecabMessage = (message: WebViewMessageEvent) => {
+    const parsedMessage = JSON.parse(message.nativeEvent.data) as MecabMessage;
+
+    if (parsedMessage.type === 'initialized') {
+      mecabInitRef.current.mecabInitPromiseResolve();
+      return;
+    }
+
+    const currentMecabCache = mecabCacheRef.current;
+    const cachedItem = currentMecabCache.get(parsedMessage.data.query);
+
+    if (!cachedItem) {
+      return;
+    }
+
+    cachedItem[0](parsedMessage.data.result);
+    currentMecabCache.delete(parsedMessage.data.query);
+  };
+
+  const mecabQuery = async (query: string): Promise<MecabMorpheme[]> => {
+    await mecabInitRef.current.mecabInitPromise;
+
+    const currentMecabWebView = mecabWebViewRef.current;
+    const currentMecabCache = mecabCacheRef.current;
+
+    if (!currentMecabWebView) {
+      return await Promise.resolve([]);
+    }
+
+    const cachedItem = currentMecabCache.get(query);
+
+    if (cachedItem) {
+      return cachedItem[1];
+    }
+
+    let resolvePromise!: (morphemes: MecabMorpheme[]) => void;
+    const promise = new Promise<MecabMorpheme[]>(resolve => {
+      resolvePromise = resolve;
+    });
+
+    currentMecabCache.set(query, [resolvePromise, promise]);
+    currentMecabWebView.postMessage(query);
+
+    return await promise;
+  };
+
   let currentStateComponent = <></>;
+
+  const mecabWebViewComponent = (
+    <WebView
+      ref={webView => setMecabWebView(webView)}
+      source={{uri: 'https://infiniterain.github.io/cdn/mecab'}}
+      originWhitelist={['*']}
+      javaScriptEnabled={true}
+      onMessage={onMecabMessage}
+      style={styles.mecabWebView}
+    />
+  );
 
   if (currentUser) {
     switch (currentPage) {
@@ -54,9 +165,10 @@ export const App = () => {
         setCurrentPage,
         batch,
         setBatch,
+        mecabQuery,
       }}>
       {currentStateComponent}
-
+      <View style={styles.mecabWebViewContainer}>{mecabWebViewComponent}</View>
       <Toast />
     </AppStateContext.Provider>
   );
