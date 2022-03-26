2c65edcfff5059fd4135b18178f1b6089a35d87b
InfiniteRain <lysenkodavid@gmail.com>
2022-03-26 18:28:38 +0200

    Remove Mecab in favour of new Kotu fields

--- src/contexts/mecab-context.ts
+++
@@ -1,8 +0,0 @@
-import {createContext} from 'react';
-import {MecabMorpheme} from '../types';
-
-type MecabContext = {
-  mecabQuery: (query: string) => Promise<MecabMorpheme[]>;
-};
-
-export const MecabContext = createContext<MecabContext>({} as MecabContext);
--- src/queries.ts
+++ src/queries.ts
@@ -1,14 +1,12 @@
-import {katakanaToHiragana} from './helpers';
-import {MecabMorpheme, Morpheme} from './types';
+import {Morpheme} from './types';
 import auth from '@react-native-firebase/auth';
 
 type KotuResponse = {
   accentPhrases: {
     components: {
       surface: string;
-      frequencySurface: string;
-      kana: string;
-      original: string;
+      surfaceOriginal: string;
+      originalKana: string;
     }[];
   }[];
 }[];
@@ -17,10 +15,7 @@ const sentenceBaseApiUrl =
   'https://us-central1-sentence-base.cloudfunctions.net/api/v1';
 const kotuUrl = 'https://kotu.io/api/dictionary/parse';
 
-export const kotuQuery = async (
-  query: string,
-  mecabQuery: (query: string) => Promise<MecabMorpheme[]>,
-): Promise<Morpheme[]> => {
+export const kotuQuery = async (query: string): Promise<Morpheme[]> => {
   const response = await fetch(kotuUrl, {method: 'post', body: query});
   const sentences = (await response.json()) as KotuResponse;
   const components = sentences
@@ -29,23 +24,10 @@ export const kotuQuery = async (
 
   const morphemes: Morpheme[] = [];
   for (const component of components) {
-    const surface = component.surface.trim();
-    const frequencySurface = component.frequencySurface.trim();
-    const dictionaryForm = frequencySurface || surface;
-    const mecabMorphemes = await mecabQuery(dictionaryForm);
-    const isParticle =
-      mecabMorphemes.length === 1 &&
-      ['助動詞', '助詞'].includes(mecabMorphemes[0]?.pos);
-
     morphemes.push({
-      surface: surface,
-      dictionaryForm: frequencySurface || surface,
-      reading: !isParticle
-        ? mecabMorphemes
-            .map(morpheme => katakanaToHiragana(morpheme.reading ?? surface))
-            .join('')
-            .trim()
-        : surface,
+      surface: component.surface.trim(),
+      dictionaryForm: (component.surfaceOriginal || component.surface).trim(),
+      reading: (component.originalKana || component.surface).trim(),
     });
   }
 
