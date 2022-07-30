46a2a3b8cb10a35ca9d3adf5a3354fac2f7571df
InfiniteRain <lysenkodavid@gmail.com>
2022-07-30 23:59:31 +0300

    refactor: Refactor ClipboardListener swift code

--- ios/ClipboardListener.swift
+++ ios/ClipboardListener.swift
@@ -4,57 +4,51 @@ import UIKit
 
 @objc(ClipboardListener)
 class ClipboardListener: RCTEventEmitter {
-  var timer: Timer!
-  let pasteboard: UIPasteboard = .general
-  var lastChangeCount: Int = 0
-  var hasListener: Bool = false
-
-  override init() {
-    super.init()
-
-    timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { [weak self] (t) in
-      guard let self = self else { return }
-      self.run()
+    lazy var timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { [weak self] t in
+        guard let self = self else { return }
+        self.run()
     }
-  }
+    let pasteboard = UIPasteboard.general
+    var lastChangeCount = 0
+    var hasListener = false
 
-  func run() {
-    if lastChangeCount == pasteboard.changeCount || !hasListener {
-      return
-    }
+    func run() {
+        guard lastChangeCount != pasteboard.changeCount && hasListener else {
+            return
+        }
 
-    let diff = pasteboard.changeCount - lastChangeCount
-    lastChangeCount = pasteboard.changeCount
+        let diff = pasteboard.changeCount - lastChangeCount
+        lastChangeCount = pasteboard.changeCount
 
-    if diff != 1 {
-      return
+        if diff != 1 {
+            return
+        }
+
+        sendEvent(withName: "clipboardUpdate", body: UIPasteboard.general.string ?? "")
     }
 
-    sendEvent(withName: "clipboardUpdate", body: UIPasteboard.general.string ?? "")
-  }
+    deinit {
+        timer.invalidate()
+    }
 
-  deinit {
-    timer.invalidate()
-  }
+    override func startObserving() {
+        hasListener = true
+    }
 
-  override func startObserving() {
-    hasListener = true
-  }
+    override func stopObserving() {
+        hasListener = false
+    }
 
-  override func stopObserving() {
-    hasListener = false
-  }
+    static override func moduleName() -> String! {
+        "ClipboardListener"
+    }
 
-  static override func moduleName() -> String! {
-    "ClipboardListener"
-  }
+    static override func requiresMainQueueSetup() -> Bool {
+        true
+    }
 
-  static override func requiresMainQueueSetup() -> Bool {
-    true
-  }
-
-  @objc
-  override func supportedEvents() -> [String]! {
-    ["clipboardUpdate"]
-  }
+    @objc
+    override func supportedEvents() -> [String]! {
+        ["clipboardUpdate"]
+    }
 }
